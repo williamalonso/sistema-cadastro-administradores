ad04426fee848d4f22931dfb867e6e7659b59557
William Alonso <williamalonso@outlook.com>
2022-10-08 19:36:21 -0300

    :zap: initial commit

---
+++ src/main/java/com/web/admin/SpringWeb/controllers/HomeController.java
@@ -0,0 +1,16 @@
+package com.web.admin.SpringWeb.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+    @RequestMapping("/")
+	public String index(Model model) { //esse "Model model" faz com que a gente possa adicionar dados que vamos enviar para nosso template html
+
+		model.addAttribute("nome", "William");
+
+		return "home/index";
+	}
+}
