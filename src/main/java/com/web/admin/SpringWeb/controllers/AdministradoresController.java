74a2087ea923fa66276d9c49728a3db4874314e0
William Alonso <williamalonso@outlook.com>
2022-10-10 21:49:42 -0300

    :zap: creating update system

--- src/main/java/com/web/admin/SpringWeb/controllers/AdministradoresController.java
+++ src/main/java/com/web/admin/SpringWeb/controllers/AdministradoresController.java
@@ -1,6 +1,7 @@
 package com.web.admin.SpringWeb.controllers;
 
 import java.util.List;
+import java.util.Optional;
 
 import com.web.admin.SpringWeb.models.Administrador;
 import com.web.admin.SpringWeb.repositorio.AdministradoresRepo;
@@ -27,7 +28,7 @@ public class AdministradoresController {
 
         List<Administrador> administradores = (List<Administrador>)repo.findAll(); // a variável "administradores" é do tipo List, ou seja, na verdade é uma lista e não variável, e busca todos os dados do Banco. O "List<Administrador>" na direita é um casting para ele trazer os resultados de findAll() e converter para o tipo List.
 
-        model.addAttribute("administradores", administradores); // O primeiro atributo "administradores" é o atributo que vamos enviar para o html. O seguno parâmetro que vamos enviar é uma lista com os dados do Banco
+        model.addAttribute("administradores", administradores); // O primeiro atributo "administradores" é o atributo que vamos enviar para o html. O segundo parâmetro que vamos enviar é uma lista com os dados do Banco
 
         return "administradores/index";
     }
@@ -37,16 +38,53 @@ public class AdministradoresController {
         return "administradores/novo";
     }
 
+    // Função que cria dado no Banco
     @PostMapping("/administradores/criar")
     public String criar(Administrador administrador) { // usando o "Administrador administrador" o Spring já pega os dados do formulário e converte para um objeto do tipo Administrador, pronto para uso.
         repo.save(administrador); // aqui estamos salvando o objeto "administrador" no Banco, usando a variável "repo" declarada lá em cima
         return "redirect:/administradores"; // após salvar, redireciona para minha lista (tabela do html)
     }
 
+    // Função que exclui
     @GetMapping("/administradores/{id}/excluir")
     public String excluir(@PathVariable int id) { // o PathVariable faz com que eu possa receber o id
         repo.deleteById(id); // deleta o dado no Banco
         return "redirect:/administradores";
     }
+
+    // Função que busca dados de um ID
+    @GetMapping("/administradores/{id}/buscar")
+    public String busca(@PathVariable int id, Model model) { // o PathVariable faz com que eu possa receber o id
+        Optional<Administrador> administrador = (Optional<Administrador>)repo.findById(id); // Busca os dados de um ID. Como o Banco pode retornar nulo, temos que usar o "Optional"
+        
+        try { // esse try catch é para o caso de o "administrador.get()" der erro na busca, por exemplo, se tentar buscar um ID muito alto e que não existe no Banco
+
+            model.addAttribute("administrador", administrador.get()); // O primeiro atributo "administrador" é o atributo que vamos enviar para o html. O segundo parâmetro é o que busca os dados do banco na linha acima. Aqui estamos usando o ".get()" porque usamos o "Optional" acima, ou seja, passamos o Optional ao invés do objeto Administrador.
+
+        } catch(Exception err) {
+
+            return "redirect:/administradores";
+
+        }
+        return "redirect:/administradores";
+    }
     
+    // Função que atualiza um ID no Banco
+    @PostMapping("/administradores/{id}/atualizar")
+    public String atualiza(@PathVariable int id, Administrador administrador) {
+        
+        // Exemplo usando query escrita na mão
+        // if(!repo.exist(id)) { // Se não existir o 'id' no Banco, redireciona para a página principal. Aqui estamos usando um método criando a query lá no "AdministradoresRepo"
+        //     return "redirect:/administradores";
+        // }
+
+        // usando um método CRUD já existente
+        if(!repo.existsById(id)) { // Se não existir o 'id' no Banco, redireciona para a página principal.
+            return "redirect:/administradores";
+        }
+
+        repo.save(administrador);  // Se estiver tudo ok, atualizamos o objeto no Banco
+        return "redirect:/administradores";
+        
+    }
 }
--- src/main/java/com/web/admin/SpringWeb/repositorio/AdministradoresRepo.java
+++ src/main/java/com/web/admin/SpringWeb/repositorio/AdministradoresRepo.java
@@ -1,5 +1,6 @@
 package com.web.admin.SpringWeb.repositorio;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 import com.web.admin.SpringWeb.models.Administrador;
@@ -11,5 +12,6 @@ import com.web.admin.SpringWeb.models.Administrador;
 // O 'Integer' significa que vamos ter um autoincrement para que a interface possa tratar os dados com inteiros
 
 public interface AdministradoresRepo extends CrudRepository<Administrador, Integer> {
-    
+    //@Query(value="select CASE WHEN count(1) > 0 THEN 'true' ELSE 'false' END from administradores where id = :id", nativeQuery = true) // Aqui estamos usando uma query direto no Banco que verifica se existe o 'id' informado
+    //public boolean exist(int id); // função que invoca a query acima
 }
